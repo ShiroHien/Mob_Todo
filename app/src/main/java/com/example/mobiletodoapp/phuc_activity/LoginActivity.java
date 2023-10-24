1b625e796a18452eb6877620773eb2726474dee2
jeikai <98737616+jeikai@users.noreply.github.com>
2023-10-24 16:07:05 +0700

    fix

--- app/src/main/java/com/example/mobiletodoapp/phuc_activity/LoginActivity.java
+++ app/src/main/java/com/example/mobiletodoapp/phuc_activity/LoginActivity.java
@@ -2,15 +2,28 @@ package com.example.mobiletodoapp.phuc_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.mobiletodoapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView logupDirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logupDirectText = findViewById(R.id.logupDirectText);
+        logupDirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, LogupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
