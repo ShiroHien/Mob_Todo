2b64527b2f1d0fc5af9c81e92018bc6be5637a80
jeikai <98737616+jeikai@users.noreply.github.com>
2023-10-24 15:54:11 +0700

    fix ui

--- app/src/main/java/com/example/mobiletodoapp/phuc_activity/LoginActivity.java
+++ app/src/main/java/com/example/mobiletodoapp/phuc_activity/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.example.mobiletodoapp.phuc_activity;
 
-import android.os.Bundle;
-import com.example.mobiletodoapp.R;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
+import com.example.mobiletodoapp.R;
+
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.activity_login);
     }
 }
