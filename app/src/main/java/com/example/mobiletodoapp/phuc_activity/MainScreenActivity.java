6fab7fc277ce2676b29079159951f6ab8d7fa78c
jeikai <98737616+jeikai@users.noreply.github.com>
2023-11-02 09:30:38 +0700

    add button

---
+++ app/src/main/java/com/example/mobiletodoapp/phuc_activity/MainScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobiletodoapp.phuc_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobiletodoapp.R;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
