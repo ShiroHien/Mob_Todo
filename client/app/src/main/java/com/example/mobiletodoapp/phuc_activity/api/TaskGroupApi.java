130f8a3a6fc2821ce7adb19acbda8a5197522150
Nguyen Van Thuyen <90054254+Thuyennnn@users.noreply.github.com>
2023-11-24 17:41:56 +0700

    them api add taskgroup

--- client/app/src/main/java/com/example/mobiletodoapp/phuc_activity/api/TaskGroupApi.java
+++ client/app/src/main/java/com/example/mobiletodoapp/phuc_activity/api/TaskGroupApi.java
@@ -2,11 +2,18 @@ package com.example.mobiletodoapp.phuc_activity.api;
 
 import com.example.mobiletodoapp.phuc_activity.dto.TaskGroup;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface TaskGroupApi {
     @POST("taskgroup/createTaskGroup")
     Call<Boolean> createTaskGroup(@Body TaskGroup data);
+
+    @GET("taskgroup/getListGroup/{userId}")
+    Call<List<TaskGroup>> getTasksGroup(@Path("userId") String userId);
 }
