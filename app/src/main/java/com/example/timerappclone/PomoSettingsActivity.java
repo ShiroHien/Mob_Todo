2b405f8eff447202625e11d93f41c6a04ab7c1d2
Shiro Hien <thaohien0607@gmail.com>
2023-11-27 22:35:04 +0700

    change ui

--- app/src/main/java/com/example/timerappclone/PomoSettingsActivity.java
+++ app/src/main/java/com/example/timerappclone/PomoSettingsActivity.java
@@ -10,41 +10,4 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PomoSettingsActivity extends AppCompatActivity {
 
-    private EditText workDurationEditText, breakDurationEditText;
-    private Button saveSettingsButton;
-
-    private SharedPreferences sharedPreferences;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.pomo_settings);
-
-        // Initialize views
-        workDurationEditText = findViewById(R.id.workDurationEditText);
-        breakDurationEditText = findViewById(R.id.breakDurationEditText);
-        saveSettingsButton = findViewById(R.id.saveSettingsButton);
-
-        // Load saved settings
-        sharedPreferences = getSharedPreferences("PomodoroSettings", MODE_PRIVATE);
-        workDurationEditText.setText(sharedPreferences.getString("workDuration", "25"));
-        breakDurationEditText.setText(sharedPreferences.getString("breakDuration", "5"));
-
-        // Save button click listener
-        saveSettingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveSettings();
-            }
-        });
-    }
-
-    private void saveSettings() {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("workDuration", workDurationEditText.getText().toString());
-        editor.putString("breakDuration", breakDurationEditText.getText().toString());
-        editor.apply();
-
-        finish();
-    }
 }
